f1ba5eb4ccc65125be79aa8e8a74da6906614a6a
AntonioGarrido19 <anto.garrido.98@gmail.com>
2023-06-08 20:13:07 -0300

    Initial commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,29 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+      <span className="font-normal normal-case">Hook</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+      <span className="font-normal normal-case">Antonio</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+      <span className="font-normal normal-case">correo@correo.com</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+      <span className="font-normal normal-case">10 Diciembre 2022</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+      <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nemo molestiae sed accusamus labore? At recusandae quaerat blanditiis doloremque in dicta. Dolor eligendi beatae deleniti molestias consequatur harum, nisi fugiat?</span>
+    </p>
+  </div>
+  )
+}
+
+export default Paciente
