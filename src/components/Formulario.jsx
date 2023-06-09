5e92119d283e2276fa15d9673de63a36f890b124
AntonioGarrido19 <anto.garrido.98@gmail.com>
2023-06-09 19:07:58 -0300

    updates

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Error from "./Error";
 
 
-const Formulario = ({ pacientes, setPacientes }) => {
+const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
   const [nombre, setNombre] = useState("");
   const [propietario, setPropietario] = useState("");
   const [email, setEmail] = useState("");
@@ -10,6 +10,24 @@ const Formulario = ({ pacientes, setPacientes }) => {
   const [sintomas, setSintomas] = useState("");
 
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+      if(Object.keys(paciente).length > 0 ){
+        setNombre(paciente.nombre)
+        setPropietario(paciente.propietario)
+        setEmail(paciente.email)
+        setFecha(paciente.fecha)
+        setSintomas(paciente.sintomas)
+      }
+  }, [paciente])
+  
+  const generarId = () => {
+    const random = Math.random().toString(36).substr(2);
+    const fecha = Date.now().toString(36)
+
+    return random + fecha
+}
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,7 +49,18 @@ const Formulario = ({ pacientes, setPacientes }) => {
       sintomas,
     };
 
-    setPacientes([...pacientes, objetoPaciente]);
+    if(paciente.id){
+      objetoPaciente.id = paciente.id
+     const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id ===
+       paciente.id ? objetoPaciente : pacienteState)
+
+       setPacientes(pacientesActualizados)
+      setPaciente({})
+
+    }else {
+      objetoPaciente.id = generarId();
+      setPacientes([...pacientes, objetoPaciente]);
+    }
 
     //Reiniciar el form
     setNombre("");
@@ -147,7 +176,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-          value="Agregar Paciente"
+          value={ paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
         />
       </form>
     </div>
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,21 +1,38 @@
-import Paciente from "./Paciente"
+import { useEffect } from "react";
+import Paciente from "./Paciente";
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+
   return (
-       <div className="md:w-1/2 lg:w3/5 md:h-screen overflow-y-scroll">
+    <div className="md:w-1/2 lg:w3/5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
           <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
-            Administra tus {''}
+            Administra tus {""}
             <span className="text-indigo-600 font-bold">Pacintes y Citas</span>
           </p>
-          
-          <Paciente/>
-          <Paciente/>
-          <Paciente/>
-          <Paciente/>
 
-      </div>
-  )
-}
+          {pacientes.map((paciente) => {
+            return <Paciente 
+            key={paciente.id} 
+            paciente={paciente}
+            setPaciente={setPaciente} 
+            eliminarPaciente={eliminarPaciente}
+            />;
+          })}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar.</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default ListadoPacientes
+export default ListadoPacientes;
